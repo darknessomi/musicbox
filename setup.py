043122aedc1a933931ca6eff1bc413b83b258846
ZHANG RT <4399.omi@gmail.com>
2015-01-02 20:00:25 +0800

    增加清除用户信息退出登录功能

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'NetEase-MusicBox',
-    version = '0.1.0.1.2',
+    version = '0.1.1',
     packages = find_packages(),
 
     include_package_data = True,
