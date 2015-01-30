52a5f1c047e04be3602f64d5dad581331097a103
尘埃 <chenansic@gmail.com>
2015-01-30 13:27:27 +0800

    fix resize term  bug
    修复了调整窗口大小以后，cureses库挂掉的bug

--- src/menu.py
+++ src/menu.py
@@ -81,6 +81,7 @@ class Menu:
         self.djstack = []
         self.userid = None
         self.username = None
+        signal.signal(signal.SIGWINCH, self.change_term)
         try:
             sfile = file(Constant.conf_dir + "/flavor.json", 'r')
             data = json.loads(sfile.read())
@@ -89,7 +90,11 @@ class Menu:
             sfile.close()
         except:
             self.collection = []
-            self.account = {}
+            self.account = {
+                
+    def change_term(self,signum, frame):
+        self.ui.screen.clear()
+        self.ui.screen.refresh()
 
     def start(self):
         self.ui.build_menu(self.datatype, self.title, self.datalist, self.offset, self.index, self.step)
