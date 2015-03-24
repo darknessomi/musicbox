c8004e1752d3f8badc549178a96dfd334baca3df
ZHANG RT <4399.omi@gmail.com>
2015-03-24 17:48:12 +0800

    upgrade version

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='NetEase-MusicBox',
-    version='0.1.3.3',
+    version='0.1.3.4',
     packages=find_packages(),
 
     include_package_data=True,
