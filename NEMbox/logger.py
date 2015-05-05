1da7f899f7c3389bafce05124b7ed6a5bc6f3d33
PrKevince <wildfire_ks@foxmail.com>
2015-05-05 12:39:54 +0800

    log directory changed

--- NEMbox/logger.py
+++ NEMbox/logger.py
@@ -11,6 +11,9 @@ import const
 import os
 
 FILE_NAME = os.path.join(const.Constant.conf_dir, 'musicbox.log')
+if os.path.isdir(const.Constant.conf_dir) is False:
+    os.mkdir(const.Constant.conf_dir)
+
 
 with open(FILE_NAME, 'a+') as f:
     f.write('#' * 80)
