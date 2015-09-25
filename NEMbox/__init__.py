20c6599b12fd58be4cbe12cceedfb4fe0c26e1d5
ZHANG RT <4399.omi@gmail.com>
2015-09-25 13:45:28 +0800

    use osascript

--- NEMbox/__init__.py
+++ NEMbox/__init__.py
@@ -10,20 +10,12 @@ from menu import Menu
 import argparse
 import sys
 
-latest = Menu().check_version()
 version = "0.1.9.2"
 
 def start():
-    if latest != version:
-        curses.endwin()
-        print '有新版本了 速速更新吧'
-        print 'NetEase-MusicBox installed version:' + version
-        print 'NetEase-MusicBox latest version:' + latest
-        print 'sudo pip install --upgrade NetEase-MusicBox'
-        sys.exit()
     nembox_menu = Menu()
     try:
-        nembox_menu.start()
+        nembox_menu.start(version)
     except:
         # clean up terminal while failed
         nembox_menu.screen.keypad(1)
@@ -40,6 +32,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--version", help="show this version and exit", action="store_true")
 args = parser.parse_args()
 if args.version:
+    latest = Menu().check_version()
     curses.endwin()
     print 'NetEase-MusicBox installed version:' + version
     if latest != version:
