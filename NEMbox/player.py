ddbe2d637a1e7583bdc8d419929384bd6d13de65
Catofes <i@catofes.com>
2015-10-28 15:10:52 +0800

    Update Append_Songs Function
    update songs info in database if mp3_url is not same

--- NEMbox/player.py
+++ NEMbox/player.py
@@ -119,7 +119,7 @@ class Player:
         else:
             thread = threading.Thread(target=runInThread, args=(onExit, popenArgs['mp3_url']))
             cache_thread = threading.Thread(target=cacheSong, args=(
-            popenArgs['song_id'], popenArgs['song_name'], popenArgs['artist'], popenArgs['mp3_url']))
+                popenArgs['song_id'], popenArgs['song_name'], popenArgs['artist'], popenArgs['mp3_url']))
             cache_thread.start()
         thread.start()
         lyric_download_thread = threading.Thread(target=getLyric, args=())
@@ -180,7 +180,10 @@ class Player:
             else:
                 database_song = self.songs[str(song["song_id"])]
                 if (database_song["song_name"] != song["song_name"]
-                    or database_song["quality"] != song["quality"]):
+                    or database_song["quality"] != song["quality"]
+                    or database_song["mp3_url"] != song["mp3_url"]):
+                    if "cache" in self.songs[str(song["song_id"])].keys():
+                        song["cache"] = self.songs[str(song["song_id"])]["cache"]
                     self.songs[str(song["song_id"])] = song
         if len(datalist) > 0 and self.info["playing_mode"] == 3 or self.info["playing_mode"] == 4:
             self.generate_shuffle_playing_list()
