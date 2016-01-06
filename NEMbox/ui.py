8c494f9c82daa164f034e61248137636313daea8
yeetim <yeetims@gmail.com>
2016-01-06 17:41:30 +0800

    Use Default Color
    Use Default Color

--- NEMbox/ui.py
+++ NEMbox/ui.py
@@ -36,6 +36,7 @@ class Ui:
         self.netease = NetEase()
 
         curses.start_color()
+        curses.use_default_colors()
         curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
         curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)
         curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
