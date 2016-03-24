1c413715b3d4e976494be507e200a5b7e0a2c4c7
Xiangru Lian <xlian@gmx.com>
2016-03-23 23:57:46 -0400

    use abstract adaptor for dbus

--- NEMbox/osdlyrics.py
+++ NEMbox/osdlyrics.py
@@ -11,6 +11,7 @@ from multiprocessing import Process
 class Lyrics(QtGui.QWidget):
     def __init__(self):
         super(Lyrics, self).__init__()
+        self.__dbusAdaptor = LyricsAdapter(self)
         self.initUI()
 
     def initUI(self):
@@ -19,11 +20,6 @@ class Lyrics(QtGui.QWidget):
         self.text = u"OSD Lyrics for Musicbox"
         self.setWindowTitle("Lyrics")
         self.show()
-
-    @QtCore.pyqtSlot(str)
-    def refresh_lyrics(self, text):
-        self.text = text
-        self.repaint()
 
     def paintEvent(self, event):
         qp = QtGui.QPainter()
@@ -36,12 +32,30 @@ class Lyrics(QtGui.QWidget):
         qp.setFont(QtGui.QFont('Decorative', 16))
         qp.drawText(event.rect(), QtCore.Qt.AlignCenter, self.text)
 
+class LyricsAdapter(QtDBus.QDBusAbstractAdaptor):
+    QtCore.Q_CLASSINFO("D-Bus Interface", "local.musicbox.Lyrics")
+    QtCore.Q_CLASSINFO("D-Bus Introspection",
+    '  <interface name="local.musicbox.Lyrics">\n'
+    '    <method name="refresh_lyrics">\n'
+    '      <arg direction="in" type="s" name="lyric"/>\n'
+    '    </method>\n'
+    '  </interface>\n')
+
+    def __init__(self, parent):
+        super(LyricsAdapter, self).__init__(parent)
+
+    @QtCore.pyqtSlot(str)
+    def refresh_lyrics(self, text):
+        self.parent().text = text
+        self.parent().repaint()
+
+
 def show_lyrics():
     app = QtGui.QApplication(sys.argv)
     # lyrics_receiver = LyricsReceiver()
     lyrics = Lyrics()
     QtDBus.QDBusConnection.sessionBus().registerService('org.musicbox.Bus')
-    QtDBus.QDBusConnection.sessionBus().registerObject('/', lyrics, QtDBus.QDBusConnection.ExportAllSlots)
+    QtDBus.QDBusConnection.sessionBus().registerObject('/', lyrics)
     sys.exit(app.exec_())
 
 def show_lyrics_new_process():
