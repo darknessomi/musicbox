50bd51356e70e7eb0eceb4fb9b7c1f5084df9dd4
ZHANG RT <4399.omi@gmail.com>
2016-04-12 11:34:19 +0800

    setWindowFlags fix

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='NetEase-MusicBox',
-    version='0.2.2.1',
+    version='0.2.2.2',
     packages=find_packages(),
     install_requires=[
         'requests',
