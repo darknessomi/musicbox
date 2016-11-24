58f13a45c0faa595931ffeb3e7cb5058ae23886e
Zhang Ruiti <4399.omi@gmail.com>
2016-11-24 13:16:57 +0800

    [Release] 0.2.3.7

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='NetEase-MusicBox',
-    version='0.2.3.6',
+    version='0.2.3.7',
     packages=find_packages(),
     install_requires=[
         'requests',
