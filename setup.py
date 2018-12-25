55acf90520b9fddb1f6f64d8133e90483f2e48be
Weiliang Li <to.be.impressive@gmail.com>
2018-12-25 14:28:04 +0900

    [Fix] update readme (#777)

--- setup.py
+++ setup.py
@@ -43,6 +43,9 @@ about = {}  # type: dict
 with open(os.path.join(here, "NEMbox", "__version__.py"), "r") as f:
     exec(f.read(), about)
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name=about["__title__"],
     version=about["__version__"],
@@ -50,9 +53,25 @@ setup(
     author_email=about["__author_email__"],
     url=about["__url__"],
     description=about["__description__"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license=about["__license__"],
     packages=find_packages(),
     install_requires=["requests-cache", "pycryptodomex", "future"],
     entry_points={"console_scripts": ["musicbox = NEMbox.__main__:start"]},
     keywords=["music", "netease", "cli", "player"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: Chinese",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Multimedia :: Sound/Audio",
+    ],
 )
