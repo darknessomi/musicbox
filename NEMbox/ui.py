2397a662a09c45055a8ac03730f0aa68677265dd
tonyfettes <feihaoxiang2014@gmail.com>
2020-06-27 23:37:53 +0800

    Optimize for someone who write ':' in their comments.

--- NEMbox/ui.py
+++ NEMbox/ui.py
@@ -44,8 +44,8 @@ def break_substr(s, start, max_len=80):
 
 
 def break_str(s, start, max_len=80):
-    res = [s.split(':')[0] + "\n"]
-    s = s.split(':')[1][1:]
+    res = [s.split(':', 1)[0] + ":\n"]
+    s = s.split(':', 1)[1][1:]
     for substr in s.splitlines():
         res.append(break_substr(substr, start, max_len=80))
     return "\n{}".format(" " * start).join(res)
