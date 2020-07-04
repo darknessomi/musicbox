53a91a638451c6dcac05ec1d3d9435366476a95e
tonyfettes <feihaoxiang2014@gmail.com>
2020-07-04 19:40:35 +0800

    Fix notification on Linux.

--- NEMbox/utils.py
+++ NEMbox/utils.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 # utils.py --- utils for musicbox
 # Copyright (c) 2015-2016 omi & Contributors
+"""
+定义几个函数 写文件 通知 返回键 创建目录 创建文件
+"""
 from __future__ import (
     print_function, unicode_literals, division, absolute_import
 )
@@ -10,12 +13,8 @@ import platform
 import subprocess
 import os
 from collections import OrderedDict
-from copy import deepcopy
 
 from future.builtins import str
-"""
-定义几个函数 写文件 通知 返回键 创建目录 创建文件
-"""
 
 __all__ = [
     'utf8_data_to_file', 'notify', 'uniq', 'create_dir', 'create_file'
@@ -57,22 +56,39 @@ def utf8_data_to_file(f, data):
     else:
         f.write(data)
 
+def notify_command_osx(msg, msg_type, duration_time=None):
+    command = ["/usr/bin/osascript", "-e"]
+    tpl = 'display notification "{}" {} with title "musicbox"'
+    sound = 'sound name "/System/Library/Sounds/Ping.aiff"' if msg_type else ""
+    command.append(tpl.format(msg, sound).encode("utf-8"))
+    return command
 
-def notify(msg, msg_type=0, t=None):
+
+def notify_command_linux(msg, duration_time=None):
+    command = ["/usr/bin/notify-send"]
+    command.append(msg.encode("utf-8"))
+    if duration_time:
+        command.extend(["-t", str(duration_time)])
+    command.extend(["-h", "int:transient:1"])
+    return command
+
+
+def notify(msg, msg_type=0, duration_time=None):
+    """ Show system notification with duration t (ms) """
     msg = msg.replace('"', '\\"')
-    command = ['/usr/bin/osascript', '-e']
-    tpl = "display notification \"{}\" {} with title \"musicbox\""
-    sound = 'sound name \"/System/Library/Sounds/Ping.aiff\"' if msg_type else ''
-    command.append(tpl.format(msg, sound).encode('utf-8'))
+    if platform.system() == "Darwin":
+        command = notify_command_osx(msg, msg_type, duration_time)
+    else:
+        command = notify_command_linux(msg, duration_time)
+
     try:
         subprocess.call(command)
         return True
-    except OSError as e:
+    except OSError:
         return False
 
 
 if __name__ == "__main__":
-    notify("I'm test \"\"quote", msg_type=1, t=1000)
-    notify("I'm test 1", msg_type=1, t=1000)
-    notify("I'm test 2", msg_type=0, t=1000)
-    print(parse_keylist([48, 49, 55, 91]))
+    notify("I'm test \"\"quote", msg_type=1, duration_time=1000)
+    notify("I'm test 1", msg_type=1, duration_time=1000)
+    notify("I'm test 2", msg_type=0, duration_time=1000)
