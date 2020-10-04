f8aaed771a360373e7e7d4916ea5535598412ea9
Weiliang Li <to.be.impressive@gmail.com>
2020-10-04 15:22:11 +0900

    fix

--- NEMbox/__init__.py
+++
@@ -1,9 +0,0 @@
-from .const import Constant
-from .utils import create_dir
-from .utils import create_file
-
-create_dir(Constant.conf_dir)
-create_dir(Constant.download_dir)
-create_file(Constant.storage_path)
-create_file(Constant.log_path, default="")
-create_file(Constant.cookie_path, default="#LWP-Cookies-2.0\n")
--- NEMbox/__main__.py
+++ NEMbox/__main__.py
@@ -32,12 +32,34 @@ import argparse
 import curses
 import sys
 import traceback
+from pathlib import Path
+import toml
 
-from .__version__ import __version__ as version
 from .menu import Menu
+from .const import Constant
+from .utils import create_dir
+from .utils import create_file
+
+
+def create_config():
+    create_dir(Constant.conf_dir)
+    create_dir(Constant.download_dir)
+    create_file(Constant.storage_path)
+    create_file(Constant.log_path, default="")
+    create_file(Constant.cookie_path, default="#LWP-Cookies-2.0\n")
+
+
+def get_current_version():
+    path = Path(".").parent.parent / "pyproject.toml"
+    with open(path) as f:
+        config = toml.load(f)
+    return config["tool"]["poetry"]["version"]
 
 
 def start():
+    create_config()
+    version = get_current_version()
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-v", "--version", help="show this version and exit", action="store_true"
