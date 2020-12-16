41e7f2605e71c238cefa45234268db28de68a52f
weak_ptr <liuweiqing@donview.cn>
2020-12-16 12:51:44 +0800

    fix(api): 修复登录报错 code=250 当前登录失败，请稍后再试

--- NEMbox/api.py
+++ NEMbox/api.py
@@ -390,7 +390,7 @@ class NetEase(object):
                 rememberLogin="true",
                 clientToken=client_token,
             )
-        data = self.request("POST", path, params)
+        data = self.request("POST", path, params,custom_cookies={'os': 'pc'})
         self.session.cookies.save()
         return data
 
