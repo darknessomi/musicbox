c3bdec16f97b31836bf4d240effe3e9570a17382
chen_10 <1803012703@qq.com>
2022-01-30 21:42:16 +0800

    fix: 32 位系统无法保存 cookie 使用 MozillaCookieJar 代替 LWPCookieJar

--- NEMbox/__init__.py
+++ NEMbox/__init__.py
@@ -40,7 +40,7 @@ def create_config():
     create_dir(Constant.download_dir)
     create_file(Constant.storage_path)
     create_file(Constant.log_path, default="")
-    create_file(Constant.cookie_path, default="#LWP-Cookies-2.0\n")
+    create_file(Constant.cookie_path, default="# Netscape HTTP Cookie File\n")
 
 
 create_config()
